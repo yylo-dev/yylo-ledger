8a215ed40e853f2c61f76d599b2584ff45359018
Ubuntu <ubuntu@ip-172-26-10-8.ec2.internal>
2026-09-20 19:03:20 +0000

    chore(release): prepare Ledger 0.3.3 with package wiki publication

--- src/yylo_ledger/__init__.py
+++ src/yylo_ledger/__init__.py
@@ -19,7 +19,7 @@ License: MIT
 from .identity import migrate_environment
 migrate_environment()
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 __author__ = "JUNO AI INC."
 
 # Export main classes for easy importing
